e45dfd91f6781665f448f94c5139c03b57b67a65
farzane nazmabadi <1379farzanenazmabadi@gmail.com>
2025-04-03 13:04:47 +0330

    شیی ئثفش ذخط هد زخدفثدف

--- includes/admin.php
+++ includes/admin.php
@@ -31,4 +31,22 @@ add_action("save_post",function($post_id,$post,$update){
 
 
 
+add_filter( "the_content", function($content){
+    $post_meta_title=get_post_meta( get_the_ID(  ), "post_extra_meta_box_title", true );
+    $post_meta_description=get_post_meta( get_the_ID(  ), "post_extra_meta_box_description", true );
 
+    if($post_meta_title || $post_meta_description){
+       $post_meta_data="
+        <div class='extra-meta-box-wrapper'>
+            <h5>".$post_meta_title."</h5>
+            <p>".$post_meta_description."</p>
+        </div>
+       ";
+       $content .= $post_meta_data;
+    };
+    return $content;
+} );
+
+add_action( "wp_enqueue_scripts", function(){
+    wp_enqueue_style("view-extra-meta-box" , PLUGIN_DIR_URL."includes/asset/app.css", [], META_PLUGIN_VERSION, false );
+} );
