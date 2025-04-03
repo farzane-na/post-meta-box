64efd895e78a93b633f7f61b857f49bb97ec4625
farzane nazmabadi <1379farzanenazmabadi@gmail.com>
2025-04-03 11:06:12 +0330

    init

---
+++ meta-box.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Plugin Name:       Post Meta Box
+ * Plugin URI:        https://github.com/arminamirinasab/wp-plugin-excercises
+ * Description:       Adds a custom meta box to posts, allowing authors to enter an additional title and text. Perfect for adding extra details, summaries, or notes.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Armin Amiri Nasab
+ * Author URI:        https://github.com/arminamirinasab/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       post-extra-box
+ * Domain Path:       /languages
+ */
