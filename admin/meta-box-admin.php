d599a2a6c9e368e3c5bb5bb8368f096148e52431
farzane nazmabadi <1379farzanenazmabadi@gmail.com>
2025-04-03 12:43:26 +0330

    save info of meta box in database

--- admin/meta-box-admin.php
+++ admin/meta-box-admin.php
@@ -1,10 +1,10 @@
 
 <p>
-    <label for="post-extra-meta-b0x-title">Title</label>
+    <label class="label-meta-box" for="post-extra-meta-box-title">Title</label>
 </p>
-<input type="text" id="post-extra-meta-b0x-title" name="post-extra-meta-b0x-title" class="input-meta-box">
+<input type="text" id="post-extra-meta-box-title" name="post-extra-meta-box-title" class="input-meta-box" value="<?= $post_extra_meta_box_title ? $post_extra_meta_box_title : '' ?>">
 
 <p>
-    <label for="post-extra-meta-box-description">Description</label>
+    <label class="label-meta-box" for="post-extra-meta-box-description">Description</label>
 </p>
-<textarea name="post-extra-meta-box-description" id="post-extra-meta-box-description" class="input-meta-box"></textarea>
+<textarea cols="10" name="post-extra-meta-box-description" id="post-extra-meta-box-description" class="input-meta-box"><?= $post_extra_meta_box_description ? $post_extra_meta_box_description : '' ?></textarea>
--- includes/admin.php
+++ includes/admin.php
@@ -3,7 +3,9 @@ add_action( "add_meta_boxes",function(){
     add_meta_box(
         "post-extra-meta-box",
         "post extra meta box",
-        function(){
+        function($post){
+            $post_extra_meta_box_title=get_post_meta( $post->ID, "post_extra_meta_box_title", true );
+            $post_extra_meta_box_description=get_post_meta( $post->ID, "post_extra_meta_box_description", true );
             include PLUGIN_DIR_PATH."admin/meta-box-admin.php";
         },
         "post",
@@ -15,3 +17,18 @@ add_action( "add_meta_boxes",function(){
 add_action("admin_enqueue_scripts",function(){
     wp_enqueue_style( "post-meta-style", PLUGIN_DIR_URL."admin/asset/style.css", [], META_PLUGIN_VERSION, false );
 });
+
+
+add_action("save_post",function($post_id,$post,$update){
+    if($_POST["post-extra-meta-box-title"]!= "" && $_POST["post-extra-meta-box-description"]!= "" && $update===true){
+        update_post_meta($post_id, "post_extra_meta_box_title",$_POST["post-extra-meta-box-title"] );
+        update_post_meta($post_id, "post_extra_meta_box_description",$_POST["post-extra-meta-box-description"] );
+    }elseif($_POST["post-extra-meta-box-title"]!= "" && $_POST["post-extra-meta-box-description"]!= "" && $update!=true){
+        add_post_meta( $post_id, "post_extra_meta_box_title",$_POST["post-extra-meta-box-title"] );
+        add_post_meta( $post_id, "post_extra_meta_box_description",$_POST["post-extra-meta-box-description"] );
+    };
+},10,3);
+
+
+
+
