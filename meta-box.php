b652795a0a87f693ade5dcc8813dc013b71e0102
farzane nazmabadi <1379farzanenazmabadi@gmail.com>
2025-04-03 11:11:44 +0330

    adding header requirment and define path and url

--- meta-box.php
+++ meta-box.php
@@ -1,15 +1,24 @@
 <?php
 /*
  * Plugin Name:       Post Meta Box
- * Plugin URI:        https://github.com/arminamirinasab/wp-plugin-excercises
+ * Plugin URI:        https://github.com/farzane-na/post-meta-box
  * Description:       Adds a custom meta box to posts, allowing authors to enter an additional title and text. Perfect for adding extra details, summaries, or notes.
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            Armin Amiri Nasab
- * Author URI:        https://github.com/arminamirinasab/
+ * Author:            Farzane Nazmabadi
+ * Author URI:        https://farzanenazmabadi.ir
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       post-extra-box
+ * Text Domain:       meta-box
  * Domain Path:       /languages
  */
+
+ define("PLUGIN_DIR_PATH",plugin_dir_path( __FILE__ ));
+ define("PLUGIN_DIR_URL",plugin_dir_url( __FILE__ ));
+
+ if ( !defined( 'ABSPATH' ) ) {
+    exit;
+};
+
+require_once BUBBLE_CONTACT_PATH."includes/admin.php";
